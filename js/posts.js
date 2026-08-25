8f17e9ed9693645b01495273cab52d4f452adbea
Malik77749 <1.88926029e+08+Malik77749@users.noreply.github.com>
2026-08-25 20:57:31 +0000

    chore: refresh comments module version

--- js/posts.js
+++ js/posts.js
@@ -3,7 +3,7 @@ import { ref, push, set, get, update, remove, increment, runTransaction, query, 
 import { escapeHtml, formatTimestamp, getYouTubeEmbedUrl, showToast, parseContent } from './utils.js?v=9';
 import { showLoading, hideLoading, showView } from './ui.js?v=10';
 import { getUserName, getUserData, addNotification } from './firebase-helpers.js?v=9';
-import { loadComments } from './comments.js?v=11';
+import { loadComments } from './comments.js?v=13';
 import * as rateLimiter from './rate-limiter.js?v=9';
 import * as pagination from './pagination.js?v=10';
 import * as blockMute from './block-mute.js?v=9';
